f2fb5054de6ddc37d0e59be44191448605d77731
wolllodunlapalt <Apollohuey+1@gmail.com>
2026-06-06 11:14:04 -0500

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "over 50 members!",
+        name: "500 Servers",
         // Activity type number (0 = Playing).
-        type: 3, 
+        type: 0), 
       },
     ],
   },
