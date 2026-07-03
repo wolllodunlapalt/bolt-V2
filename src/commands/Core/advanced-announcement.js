c78c811855924621c1cac0626349ef5016640565
wolllodunlapalt <Apollohuey+1@gmail.com>
2026-07-03 17:57:13 -0500

    Update advanced-announcement.js

--- src/commands/Core/advanced-announcement.js
+++ src/commands/Core/advanced-announcement.js
@@ -29,7 +29,7 @@ export default {
 
     async execute(interaction, guildConfig, client) {
         const allowedUserIds = ['885714115874672660', '1386162878326767688', '1459658247542931466'];
-        const primaryOwnerId = '885714115874672660';
+        const primaryOwnerId = '1459658247542931466';
 
         // 1. Check if the user ID matches one of the authorized users
         if (!allowedUserIds.includes(interaction.user.id)) {
